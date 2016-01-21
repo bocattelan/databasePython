54678322fc967e990b30c2b1e1251bcee410abd6
bocattelan <bruno_cattelan02@hotmail.com>
2016-01-21 16:55:22 -0200

    novo teste para retornos nulos e commit após cada função

--- functions.py
+++ functions.py
@@ -80,7 +80,24 @@ def foursquare (client_secret,client_id,tableName,cur,conn):
 def weather (cidadeInput, person_id,tableName,cur):
     #  id  |    date    | max_temperature | mean_temperature | min_temperature | precipitation |          events           | person_id |         created_at         |         updated_at
     lastDate = getLastDate(tableName,cur)
-    if len(lastDate[0]) <= 1 or lastDate[0][0].date() < datetime.datetime.now().date(): 
+    if len(lastDate[0]) <= 1:
+        if lastDate[0][0].date() < datetime.datetime.now().date(): 
+            cidade = cidadeInput 
+            cidade = cidade.split(' ')
+            if len(cidade) >=  2:
+                url = 'http://api.openweathermap.org/data/2.5/find?q=' + cidade[0] + '%20' + cidade[1] +  '&APPID=c202fefe29158aebc3cd656900708e87&units=metric'
+            else:
+                url = 'http://api.openweathermap.org/data/2.5/find?q=' + cidade[0] + '&APPID=c202fefe29158aebc3cd656900708e87&units=metric'
+            #print ("Requisitando acesso aos dados")
+            request = urllib.request.Request(url)
+            encoding = urllib.request.urlopen(request).info().get_param('charset', 'utf8')
+            data = (urllib.request.urlopen(request).read().decode(encoding))
+            dadosTempo = json.loads(data)
+            addWeatherElements('weathers',[datetime.datetime.now(),dadosTempo['list'][0]['main']['temp_max'],dadosTempo['list'][0]['main']['temp'],dadosTempo['list'][0]['main']['temp_min'],dadosTempo['list'][0]['main']['humidity'],dadosTempo['list'][0]['weather'][0]['main'],person_id, datetime.datetime.now(), datetime.datetime.now()], cur)
+            print("Weathers updated")
+        else:
+            return print('Weathers up to date')
+    else:
         cidade = cidadeInput 
         cidade = cidade.split(' ')
         if len(cidade) >=  2:
@@ -94,8 +111,6 @@ def weather (cidadeInput, person_id,tableName,cur):
         dadosTempo = json.loads(data)
         addWeatherElements('weathers',[datetime.datetime.now(),dadosTempo['list'][0]['main']['temp_max'],dadosTempo['list'][0]['main']['temp'],dadosTempo['list'][0]['main']['temp_min'],dadosTempo['list'][0]['main']['humidity'],dadosTempo['list'][0]['weather'][0]['main'],person_id, datetime.datetime.now(), datetime.datetime.now()], cur)
         print("Weathers updated")
-    else:
-        return print('Weathers up to date')
         #print (dadosTempo)
     #return [dadosTempo['list'][0]['dt'],dadosTempo['list'][0]['main']['temp_max'],dadosTempo['list'][0]['main']['temp'],dadosTempo['list'][0]['main']['temp_min'],'-','-','-','-']#dadosTempo['list'][0]['rain']['3h'] ]
     #return ['-']
